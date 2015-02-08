7aa240d437eec9acb85ee98028ea7b7a5b80d3a6
Dongsheng Wei <dswei1989@gmail.com>
2015-02-08 21:57:07 +0800

    fix a bug in one_failed_repair_time.sh

--- one_failed_repair_time.sh
+++ one_failed_repair_time.sh
@@ -5,7 +5,7 @@
 
 for((i=1;i<$5+1;i=i+1))
 do
-	./recover $1 $2 $3 | grep -E "Repair_time|Read|Write|Encoding" | awk '{print $4}' | awk '{printf "%s\t",$0}' >> $4
+	./recover $1 $2 $3 | grep -E "^Repair_time|^Read|^Write|^Encoding" | awk '{print $4}' | awk '{printf "%s\t",$0}' >> $4
 	echo >> $4
 done
 
